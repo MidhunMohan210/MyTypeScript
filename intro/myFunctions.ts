4f3696a2df0008963289d85dd71cb76f3fe9c012
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-07-16 22:18:55 +0530

    functions completed

---
+++ intro/myFunctions.ts
@@ -0,0 +1,19 @@
+// function addTwo(num:number):string{
+//     // return num+2;
+//     return "haii"
+// }
+
+// addTwo(6)
+
+
+// const  getHello=(s:string):string=>{
+//     return "haii"
+
+// }
+
+// const heros=[1,2,3,4,5]
+const heros=["1","2","3"]
+
+heros.map((hero:string):string=>{
+    return hero
+})
---
+++ intro/variable.ts
@@ -0,0 +1,15 @@
+// number
+
+let userId="S,JDHFSKJ";
+
+
+
+//boolean 
+
+
+let isLoggedIn:boolean =true;
+
+
+
+
+
