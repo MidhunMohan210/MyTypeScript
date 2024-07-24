95031152fef897e68110f5ffd677214ef70a5c42
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-07-24 21:06:57 +0530

    object typescript

---
+++ intro/myObjects.ts
@@ -0,0 +1,12 @@
+function createUser({name,isPaid }:{name:string,isPaid:boolean }){
+
+}
+
+createUser({name:"jja",isPaid:false})
+
+
+function hai (num:number):[number]{
+    return [num]
+}
+
+export{}
